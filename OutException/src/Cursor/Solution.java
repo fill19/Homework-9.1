fc7a1bc1f901fbcb2e79bf0b7e060bc3c445a9f9
Dmytro <65898747+fill19@users.noreply.github.com>
2020-12-22 17:36:37 +0200

    Task 9.1
    Task 9.1

---
+++ OutException/src/Cursor/Solution.java
@@ -0,0 +1,24 @@
+package Cursor;
+
+public class Solution {
+    public static void main(String[] args) {
+        int x = 3;
+
+        while (true) {
+
+            if (x < 2) {
+                System.out.print("a");
+            }
+            if (x == 2) {
+                System.out.print("b c");
+            }
+            if (x == 1)  {
+                System.out.print("d");
+                x = x - 1;
+            }
+            x = x - 1;
+            System.out.print("-");
+        }
+    }
+}
+
